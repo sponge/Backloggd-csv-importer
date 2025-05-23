7c71c79c9999809b20aeb286d244ca1ad72723a8
sponge <sponge@d8d.org>
2025-05-23 16:32:50 -0400

    format pep8

--- backloggd.py
+++ backloggd.py
@@ -1,10 +1,14 @@
-import requests, csv, json, sys, time
+import requests
+import csv
+import json
+import sys
+import time
 from datetime import datetime
 
 s = requests.Session()
 
 # get IGDB creds
-with open('backloggd.json','r') as f:
+with open('backloggd.json', 'r') as f:
     j = json.loads(f.read())
 
 id = j['id']
@@ -13,7 +17,8 @@ backloggd_id = j['backloggd_id']
 backloggd_csrf = j['csrf']
 backloggd_cookie = j['cookie']
 
-access_url = 'https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials' % (id, secret)
+access_url = 'https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials' % (
+    id, secret)
 r = s.post(access_url)
 response = json.loads(r.text)
 
@@ -23,56 +28,62 @@ endpoint = 'https://api.igdb.com/v4/games/'
 headers = {'Client-ID': id, 'Authorization': 'Bearer ' + access_token}
 
 BACKLOGGD_HEADERS = {
-  'Connection': 'keep-alive',
-  'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-  'Accept': '*/*',
-  'X-CSRF-Token': '',
-  'X-Requested-With': 'XMLHttpRequest',
-  'sec-ch-ua-mobile': '?0',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'Origin': 'https://www.backloggd.com',
-  'Sec-Fetch-Site': 'same-origin',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Dest': 'empty',
-  'Referer': 'https://www.backloggd.com/',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'Cookie': '',
+    'Connection': 'keep-alive',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+    'Accept': '*/*',
+    'X-CSRF-Token': '',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Origin': 'https://www.backloggd.com',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://www.backloggd.com/',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cookie': '',
 }
+
 
 def update_cookie(session):
     BACKLOGGD_HEADERS['Cookie'] = "ne_cookies_consent=true; _backloggd_session=" + session
 
+
 def update_csrf(key):
     BACKLOGGD_HEADERS['X-CSRF-Token'] = key
 
+
 def get_game_id(name, platform):
     try:
-        body = 'fields name; search "%s"; where platforms = (%s);' % (name, platform)
+        body = 'fields name; search "%s"; where platforms = (%s);' % (
+            name, platform)
         r = s.post(endpoint, headers=headers, data=body)
         j = json.loads(r.text)
         actual_game = [g['id'] for g in j]
         if len(actual_game) > 0:
-            return actual_game[0] # this is the ID
+            return actual_game[0]  # this is the ID
         else:
-            return None # game not found
+            return None  # game not found
     except:
         print("Error getting game id " + name)
         return None
 
+
 def get_plaform_id():
-    platforms_endpont='https://api.igdb.com/v4/platforms'
+    platforms_endpont = 'https://api.igdb.com/v4/platforms'
     try:
         body = 'fields name; limit 300;'
         r = s.post(platforms_endpont, headers=headers, data=body)
         j = json.loads(r.text)
         if len(j) > 0:
-            return j # this is the ID
+            return j  # this is the ID
         else:
-            return None # game not found
+            return None  # game not found
     except:
         print("Error getting platform names")
-        return None        
+        return None
+
 
 def add_game(game_id, rating, platform, is_play, is_playing, is_backlog, is_wishlist, status):
     data = {
@@ -93,11 +104,13 @@ def add_game(game_id, rating, platform, is_play, is_playing, is_backlog, is_wish
         'log[is_playing]': is_playing,
         'log[is_backlog]': is_backlog,
         'log[is_wishlist]': is_wishlist,
-        'log[status]': status, # played, completed (default), retired, shelved, abandoned
+        # played, completed (default), retired, shelved, abandoned
+        'log[status]': status,
         'log[id]': '',
         'modal_type': 'quick'
     }
-    backloggd_url = 'https://www.backloggd.com/api/user/' + str(backloggd_id) + '/log/' + str(game_id)
+    backloggd_url = 'https://www.backloggd.com/api/user/' + \
+        str(backloggd_id) + '/log/' + str(game_id)
     add_request = s.post(backloggd_url, headers=BACKLOGGD_HEADERS, params=data)
     return add_request.status_code
 
@@ -107,10 +120,10 @@ def add_game(game_id, rating, platform, is_play, is_playing, is_backlog, is_wish
 update_cookie(backloggd_cookie)
 update_csrf(backloggd_csrf)
 platforms = get_plaform_id()
-not_found_games = open('notfound.txt','w')
+not_found_games = open('notfound.txt', 'w')
 start_from_row = 1
 index = 0
-with open('games.csv','r') as csvfile:
+with open('games.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         if index < start_from_row:
@@ -129,13 +142,14 @@ with open('games.csv','r') as csvfile:
             platform = next(i for i in platforms if i['name'] == platform_name)
             game_id = get_game_id(name, platform['id'])
             if game_id is not None:
-                status = add_game(game_id, rating, platform['id'], is_play, is_playing, is_backlog, is_wishlist, status)
+                status = add_game(
+                    game_id, rating, platform['id'], is_play, is_playing, is_backlog, is_wishlist, status)
                 trying = False
                 if status < 400:
                     print('Added ' + name)
                 elif status == 429:
                     print('Hit request limit, pausing')
-                    trying = True # try again
+                    trying = True  # try again
                     time.sleep(60*3)
                     print('Trying again')
                 else:
